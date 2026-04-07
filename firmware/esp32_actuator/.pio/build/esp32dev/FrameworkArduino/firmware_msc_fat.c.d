9b26c6907fd4fa2132258174bb03d081e82db2df
JorgeeAC <jjjcarlos12321@gmail.com>
2026-04-07 00:12:50 -0500

    branch cleanup

--- firmware/esp32_actuator/.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
- /tmp/platformio-core/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- /tmp/platformio-core/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
--- firmware/esp32_actuator/.pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.o: \
- /tmp/platformio-core/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- /tmp/platformio-core/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
